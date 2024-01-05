4456259fa12b7b168dd06b500181f761c6912e2c
ViralSir <viralsir2018@gmail.com>
2024-01-05 17:42:04 +0530

    Lecture - 6 airline app has been added and orm has been introduced.

--- airline/models.py
+++ airline/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class flight(models.Model):
+class flight(models.Model) :
     source=models.CharField(max_length=40)
     destination=models.CharField(max_length=40)
     duration=models.CharField(max_length=20)
